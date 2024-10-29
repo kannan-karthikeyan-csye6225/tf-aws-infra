4ea6e604e03812a3facf74e1d6ba6b1e065fa6aa
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-10-29 16:31:34 -0400

    Configure Cloud Watch

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,6 +5,8 @@ resource "aws_instance" "web_app" {
   vpc_security_group_ids      = [var.app_security_group_id]
   associate_public_ip_address = true
 
+  iam_instance_profile = "CloudWatchAgent"
+
   user_data = base64encode(<<-EOF
               #!/bin/bash
               echo "DB_NAME=csye6225" >> /opt/apps/webapp/.env
@@ -12,8 +14,16 @@ resource "aws_instance" "web_app" {
               echo "DB_PASSWORD=${var.db_password}" >> /opt/apps/webapp/.env
               echo "DB_HOST=${var.db_endpoint}" >> /opt/apps/webapp/.env
               echo "DB_PORT=5432" >> /opt/apps/webapp/.env
-              
+
+              /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+                -a fetch-config \
+                -m ec2 \
+                -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+                -s
+
               sudo systemctl restart myapp
+              sudo systemctl restart amazon-cloudwatch-agent
+              
               EOF
   )
 
