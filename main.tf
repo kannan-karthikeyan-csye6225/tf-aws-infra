0ae9d6f214d50e3059c74f2e0cb006cf7d685fde
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-11-05 00:42:08 -0500

    Fix Formatting

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 }
 
 module "vpc" {
-  source              = "./modules/vpc"
-  vpc_cidr_block      = var.vpc_cidr_block
-  public_subnet_cidrs = var.public_subnet_cidrs
+  source               = "./modules/vpc"
+  vpc_cidr_block       = var.vpc_cidr_block
+  public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
-  availability_zones  = var.availability_zones
+  availability_zones   = var.availability_zones
 }
 
 module "sg" {
@@ -18,22 +18,22 @@ module "sg" {
 }
 
 module "rds" {
-  source              = "./modules/rds"
-  private_subnet_ids  = module.vpc.private_subnet_ids
+  source               = "./modules/rds"
+  private_subnet_ids   = module.vpc.private_subnet_ids
   db_security_group_id = module.sg.db_security_group_id
-  db_password        = var.db_password
+  db_password          = var.db_password
 }
 
 module "ec2" {
-  source               = "./modules/ec2"
-  custom_ami           = var.custom_ami
-  public_subnet_id     = module.vpc.public_subnet_ids[0]
+  source                = "./modules/ec2"
+  custom_ami            = var.custom_ami
+  public_subnet_id      = module.vpc.public_subnet_ids[0]
   app_security_group_id = module.sg.app_security_group_id
-  db_password         = var.db_password
-  db_endpoint         = module.rds.db_endpoint
-  aws_region          = var.aws_region 
-  bucket_name         = module.s3.bucket_name
-  bucket_arn          = module.s3.bucket_arn
+  db_password           = var.db_password
+  db_endpoint           = module.rds.db_endpoint
+  aws_region            = var.aws_region
+  bucket_name           = module.s3.bucket_name
+  bucket_arn            = module.s3.bucket_arn
 }
 
 module "s3" {
